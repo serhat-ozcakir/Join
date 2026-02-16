57ccadf14cc0f8f311ed80150b065f08ac599097
serhat özcakir <serhatozcakir35@gmail.com>
2026-02-16 15:35:03 +0100

    Contact component edited

--- src/app/features/contacts/pages/contacts-page/contacts-page.ts
+++ src/app/features/contacts/pages/contacts-page/contacts-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactList } from '../../components/contact-list/contact-list';
+import { ContactDetail } from '../../components/contact-detail/contact-detail';
+import { ContactFormDialog } from '../../components/contact-form-dialog/contact-form-dialog';
 
 @Component({
   selector: 'app-contacts-page',
-  imports: [],
+  imports: [ContactList,ContactDetail,ContactFormDialog ],
   templateUrl: './contacts-page.html',
   styleUrl: './contacts-page.scss',
 })
