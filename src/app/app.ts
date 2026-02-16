2ca93ddcb55a4c8ec1914fc830ea01fca79d8eb7
vadyk <vadykdeejay@gmail.com>
2026-02-16 10:16:13 +0100

    db join connected

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Supabase } from './supabase';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('join-project');
+  dbJoin= inject(Supabase)
 }
--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Supabase {
-  
+supabaseUrl = 'https://rtunkmriznurqroovzij.supabase.co'
+supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0dW5rbXJpem51cnFyb292emlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTI4MjAsImV4cCI6MjA4Njc4ODgyMH0.J4bDrpH72a81aHGBdHvT5Vrl30NgoZTOB8wvAHwmIoE'
+supabase = createClient(this.supabaseUrl, this.supabaseKey)
 }
