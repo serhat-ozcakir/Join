c24f4514f7433e5f4c9e14c726f23d50940482f9
Benjamin Blarr <b.blarr@gmx.de>
2026-02-16 16:05:19 +0100

    Implemented header and sidebar to contacts

--- src/app/features/contacts/pages/contacts-page/contacts-page.ts
+++ src/app/features/contacts/pages/contacts-page/contacts-page.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ContactList } from '../../components/contact-list/contact-list';
 import { ContactDetail } from '../../components/contact-detail/contact-detail';
 import { ContactFormDialog } from '../../components/contact-form-dialog/contact-form-dialog';
+import { Header } from '../../../../shared/components/header/header';
+import { Sidebar } from '../../../../shared/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-contacts-page',
-  imports: [ContactList,ContactDetail,ContactFormDialog ],
+  imports: [ContactList,ContactDetail,ContactFormDialog, Header, Sidebar ],
   templateUrl: './contacts-page.html',
   styleUrl: './contacts-page.scss',
 })
