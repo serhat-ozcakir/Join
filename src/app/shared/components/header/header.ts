e5c6c705cabf091d883e8e8606fbc2fee2161c3e
Benjamin Blarr <b.blarr@gmx.de>
2026-02-17 14:17:09 +0100

    Fixed font error

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
