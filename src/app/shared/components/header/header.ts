a6ff9cb79a6b4c2c6570c5f1806574fe8c25cc30
vadyk <vadykdeejay@gmail.com>
2026-02-17 14:38:14 +0100

    name in header added

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Supabase } from '../../../supabase';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,16 @@ import { RouterLink } from "@angular/router";
   styleUrl: './header.scss',
 })
 export class Header {
+ supabase = inject(Supabase);
+
+  getInitials(): string {
+    const user = this.supabase.currentUser();
+    if (!user) return '?';
+    const name = user.user_metadata?.['display_name'] || user.email || '';
+    return name.split(' ').map((n: string) => n[0]).join('').toUpperCase().slice(0, 2) || '?';
+  }
+
+
 isMenuOpen = false;
 
   toggleMenu(){
