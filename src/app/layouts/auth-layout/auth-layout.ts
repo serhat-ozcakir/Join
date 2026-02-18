cfaf07db37b9b755a131074cd77ee7f5329f6f0f
serhat özcakir <serhatozcakir35@gmail.com>
2026-02-18 15:08:52 +0100

    auth-layout pages updated and  updateIsSignupPage() written

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -1,12 +1,22 @@
 import { Component, } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
 export class AuthLayout {
+  isSignupPage = false;
 
+ constructor(private router: Router) {
+  this.updateIsSignupPage()
+  this.router.events.pipe(filter((e)=> e instanceof NavigationEnd)).subscribe(()=>
+  this.updateIsSignupPage())
+ }
+ private updateIsSignupPage(){
+  this.isSignupPage = this.router.url.startsWith('/signup')
+ }
 }
