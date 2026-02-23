3b40f787575423da5e4aa668a2c053d5a4995159
serhat özcakir <serhatozcakir35@gmail.com>
2026-02-23 16:43:44 +0100

    model created and types written

---
+++ src/app/features/board/components/task-card/task-card.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-card',
+  imports: [CommonModule],
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.scss',
+})
+export class TaskCard {
+   @Input({ required: true }) task!: Task;
+
+   get doneSubtasksCount(): number {
+    if (!this.task.subtasks) {
+      return 0;
+    }
+    return this.task.subtasks.filter(sub => sub.done).length;
+  }
+
+  get totalSubtasksCount(): number {
+    return this.task.subtasks ? this.task.subtasks.length : 0;
+  }
+}
