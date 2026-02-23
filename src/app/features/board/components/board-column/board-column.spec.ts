4d72db97059674f75b9a46cb7efad4ee0bce9b61
serhat özcakir <serhatozcakir35@gmail.com>
2026-02-23 16:45:47 +0100

    Board-column component created and edited

---
+++ src/app/features/board/components/board-column/board-column.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardColumn } from './board-column';
+
+describe('BoardColumn', () => {
+  let component: BoardColumn;
+  let fixture: ComponentFixture<BoardColumn>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BoardColumn]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BoardColumn);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/board/components/board-column/board-column.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskCard } from '../task-card/task-card';
+import { Task } from '../../models/task.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board-column',
+  standalone: true,
+  imports: [CommonModule, TaskCard],
+  templateUrl: './board-column.html',
+  styleUrl: './board-column.scss',
+})
+export class BoardColumn {
+  @Input() title = '';
+  @Input() tasks: Task[] = [];
+
+  @Output() taskSelected = new EventEmitter<Task>();
+  @Output() addClicked = new EventEmitter<void>();
+
+  onTaskSelected(task: Task): void {
+    this.taskSelected.emit(task);
+  }
+
+  onAddTask(): void {
+    this.addClicked.emit();
+    console.log('working');
+  }
+}
