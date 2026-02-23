865a59183bb7a8dd0c4b39620defb61c4c68e119
serhat özcakir <serhatozcakir35@gmail.com>
2026-02-23 16:47:51 +0100

    Task model ts and type created and edited

---
+++ src/app/features/board/models/task.model.ts
@@ -0,0 +1,29 @@
+// task.model.ts
+export type Status = 'todo' | 'inProgress' | 'awaitFeedback' | 'done';
+export type TaskType = 'User Story' | 'Technical Task';
+export type TaskPriority = 'urgent' | 'medium' | 'low';
+
+export interface Assignee {
+  id: string;
+  initials: string;
+  name?: string;
+}
+
+export interface Subtask {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+export interface Task {
+  id: string;
+  title: string;
+  description?: string;
+  status: Status;
+  type: TaskType;
+  priority: TaskPriority;
+  assignees?: Assignee[];
+  subtasks?: Subtask[];
+  createdAt: string; 
+  dueDate?: string; 
+}
