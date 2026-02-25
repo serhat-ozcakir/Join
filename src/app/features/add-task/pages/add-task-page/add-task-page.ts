eaa4c6663ff630b57403385d2a2b355780297bb3
hello90343 <za.gr@web.de>
2026-02-25 21:41:22 +0100

    added example of validation

--- src/app/features/add-task/pages/add-task-page/add-task-page.ts
+++ src/app/features/add-task/pages/add-task-page/add-task-page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {FormBuilder,FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 /** Page for creating new tasks with title, description, priority, and assignment. */
 @Component({
   selector: 'app-add-task-page',
-  imports: [],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './add-task-page.html',
   styleUrl: './add-task-page.scss',
 })
 export class AddTaskPage {
-
+  taskForm = new FormGroup({
+    titel: new FormControl('', {
+      validators: [Validators.maxLength(20)]
+    }),
+    description: new FormControl('', {
+      validators: [Validators.maxLength(40)]
+    }),
+    date: new FormControl('', {
+      validators: [Validators.minLength(5)]
+    }),
+    category: new FormControl('', {
+      validators: [Validators.minLength(10)]
+    }),
+    subtasks: new FormControl('', {
+      validators: [Validators.minLength(1)]
+    }),
+  })
 }
